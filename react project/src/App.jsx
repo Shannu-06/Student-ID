20e3bb71544db7534e1e06ba022617122a0d3bbe
Shannu-06 <shannuch07@gmail.com>
2025-06-07 17:05:15 +0530

    project done

--- react project/src/App.jsx
+++ react project/src/App.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import StudentCard from './components/StudentCard';
+import RegistrationForm from "./components/RegistrationForm";
 
 function App() {
-  const student = {
-    name: 'Shanmukha',
-    age: 19,
-    course: 'Computer Science',
-    grade: 'A',
-    profilePic: '/shannu.jpg', // relative path from public folder
-  };
-
   return (
     <div>
-      <h1>Student Info</h1>
-      <StudentCard {...student} />
+      <RegistrationForm />
     </div>
   );
 }
 
-export default App;
+export default App;
