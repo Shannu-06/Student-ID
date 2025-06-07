cc2d4e68903af2a486e90b48b591b763a4d83e59
Shannu-06 <shannuch07@gmail.com>
2025-06-07 17:34:21 +0530

    Update App.jsx

--- react project/src/App.jsx
+++ react project/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import RegistrationForm from "./components/RegistrationForm";
+import StudentCard from './components/StudentCard';
 
 function App() {
+  const student = {
+    name: 'Shanmukha',
+    age: 19,
+    course: 'Computer Science',
+    grade: 'A',
+    profilePic: '/shannu.jpg', // relative path from public folder
+  };
+
   return (
     <div>
-      <RegistrationForm />
+      <h1>Student Info</h1>
+      <StudentCard {...student} />
     </div>
   );
 }
 
-export default App;
+export default App;
